0517c7d6c34650b86e3c90bb8d5e758a24f8ac15
srinivasGokarla <87422030+srinu217@users.noreply.github.com>
2022-03-18 18:46:45 +0530

    changed

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
 const schedule = require('node-schedule')
 
- let i = 1;
+ 
+ let out = []
+ let arr = ["Taskone", "Tasktwo", "Taskthree", "Taskfour", "Taskfive", "Tasksix", "Taskseven", "Taskeight", "Tasknine","TaskTen"];
+ for(j = 0; j < arr.length; j++) {
+    let res  = arr[j]
+   let ans = res.split("").reverse();
+   let str = ""
+   for(k= 0; k < ans.length; k++) {
+     str = str + ans[k];
+    
+   }
+   out.push(str)
+ }
+
+  //  console.log(out)
+let i = 0
  const task = schedule.scheduleJob('*/7 * * * * *', () => { 
   
-     if(i === 10) {
+     if(i === 9) {
         schedule.cancelJob(task)
      }
-    console.log(`"text":Task-${i}`)
+    console.log(`"text":${out[i]}`)
     i++
     console.log(`"date":`, new Date())
 
